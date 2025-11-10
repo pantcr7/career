9a0bf1664c1eb21fb99392de2c47ae524c1c9bc9
Prasant Pant <77143354+pantcr7@users.noreply.github.com>
2025-11-10 06:30:55 +0000

    database connection established

--- database.py
+++ database.py
@@ -1,9 +1,6 @@
-import sqlalchemy
-from sqlalchemy import create_engine, URL
-from dotenv import load_dotenv
-
 import os
-
+from sqlalchemy import create_engine, text, URL
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -12,10 +9,18 @@ url_object = URL.create(
     username=os.getenv("USERNAME"),
     password=os.getenv("PASSWORD"),
     host=os.getenv("HOST"),
+    port=os.getenv("PORT"),
     database=os.getenv("DATABASE"),
     query={"charset": "utf8mb4"},
-  )
+)
 
-engine = create_engine(url_object)
+connect_args = {
+    "ssl": {"ca": os.getenv("CA_CERT_PATH")}
+}
 
+engine = create_engine(url_object, connect_args=connect_args)
 
+# Test query
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT * FROM jobs LIMIT 5"))
+    print(result.all())
